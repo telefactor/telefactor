ef81dc65bc63434dcb5d979ea210aeb9c5b32296
Sebastian Sangervasi <villain@harmless.dev>
2019-08-26 17:55:22 -0700

    woot big old fils

--- repo-management/lib/telefactor/repo_management.rb
+++ repo-management/lib/telefactor/repo_management.rb
@@ -7,26 +7,49 @@ module Telefactor
     require 'faraday-http-cache'
     require 'octokit'
 
-    class << self
-      def octokit_client
-        stack = Faraday::RackBuilder.new do |builder|
-          builder.use Faraday::HttpCache, serializer: Marshal, shared_cache: false
-          builder.use Octokit::Response::RaiseError
-          builder.adapter Faraday.default_adapter
-        end
-        Octokit.middleware = stack
+    class Manager
+      class << self
+        def create_octokit_client
+          stack = Faraday::RackBuilder.new do |builder|
+            builder.use Faraday::HttpCache, serializer: Marshal, shared_cache: false
+            builder.use Octokit::Response::RaiseError
+            builder.adapter Faraday.default_adapter
+          end
+          Octokit.middleware = stack
 
-        Octokit::Client.new(access_token: Secrets.secrets.github.access_token)
+          Octokit::Client.new(access_token: Secrets.secrets.github.access_token)
+        end
+      end
+
+      def user
+        client.user
+      end
+
+      def repos
+        client.repositories
+      end
+
+      def telefactor_repos
+        repos.select do |repo|
+          repo.name.match?(/fam/) && !repo.fork?
+        end
+      end
+
+      def client
+        @client ||=
+          self.class.create_octokit_client.tap do |client_for_config|
+            client_for_config.auto_paginate = true
+          end
       end
     end
 
     module Secrets
       require 'dry-struct'
       require 'pathname'
-
       require 'safe_yaml'
 
       SafeYAML::OPTIONS[:default_mode] = :safe
+
       class << self
         def secrets
           @secrets ||= wrap_with_models(load_yaml_file)
