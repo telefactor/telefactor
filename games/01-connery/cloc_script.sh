e0e1afc77165de90b96116952cfcf445b69c42da
Sebastian Sangervasi <villain@harmless.dev>
2020-03-25 16:30:00 -0700

    stats

---
+++ games/01-connery/cloc_script.sh
@@ -0,0 +1,4 @@
+# tfr game exec-all -- bash -l $(get_absolute_path ./cloc_script.sh)
+test_out_dir=$(dirname $BASH_SOURCE)/stats/cloc
+mkdir -p "${test_out_dir}"
+cloc --json --out "${test_out_dir}/$(basename $PWD).json" ./src
--- games/01-connery/test_script.sh
+++ games/01-connery/test_script.sh
@@ -1,1 +1,7 @@
+# tfr game exec-all -- bash -l $(get_absolute_path ./test_script.sh)
 nvm version
+npm install --silent --offline
+
+test_out_dir=$(dirname $BASH_SOURCE)/stats/tests
+mkdir -p "${test_out_dir}"
+npm run test -- --json --outputFile "${test_out_dir}/$(basename $PWD).json"
